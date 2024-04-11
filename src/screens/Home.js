a6783bcb445ead941b29175fa23ce4e17cf07eca
ArthurBortoleto <arthurmarcelbdossantos@gmail.com>
2024-04-11 10:22:53 -0300

    Adicionando search e propiedade play/pause

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, StatusBar, FlatList} from "react-native";
+import {Audio} from "expo-av";
 import MusicItem from "../components/MusicItem";
 
 export default function Home({navigation}) {
   const [currentPlaying, setCurrentPlaying] = useState(null);
-  const [musicData, setMusicData] = useState([])
+  const [musicData, setMusicData] = useState([]);
+  const [currentSound, setCurrentSound] = useState(null);
+
+  const togglePlayPause = async (item) => {
+    if(currentSound && currentPlaying == item.id){
+      await currentSound.pauseAsync();
+      setCurrentPlaying(null);
+      setCurrentSound(null);
+    } else {
+      if(currentSound){
+        await currentSound.unloadAsync();
+      }
+      const {sound} = await Audio.Sound.createAsync(
+        {uri: `http://10.0.2.2:3000/musics/${item.music_path}`},
+        {shouldPlay: true}
+      );
+      setCurrentSound(sound);
+      setCurrentPlaying(item.id);
+    }
+  };
+
   const item = {
     id: 1,
     title: "Highway to Hell",
@@ -29,7 +50,10 @@ export default function Home({navigation}) {
       data={musicData} 
       keyExtractor={(item) => item.id.toString()} 
       renderItem={({item}) => (
-        <MusicItem isPlaying={() => currentPlaying == item.id} music={item} navigation={navigation} onPlayPause={() => {}}/>
+        <MusicItem 
+        isPlaying={currentPlaying === item.id} 
+        music={item} navigation={navigation} 
+        onPlayPause={() => togglePlayPause(item)}/>
       )}/>
     </View>
   );
